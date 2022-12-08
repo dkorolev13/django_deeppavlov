92016c2947d6ddff1493039139a88ce51747c253
Dmitry Korolev <dkorolev13@gmail.com>
2022-12-08 23:38:36 +0300

    rebname variables

--- deeppavlov/deeppavlov/settings.py
+++ deeppavlov/deeppavlov/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-0$87!g$kqaerw76i!rk1448(=3(_j9es7c8*w#)r1wu5w7bgj7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get("DEBUG", default=0))
+# DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = 1
+
 
 ALLOWED_HOSTS = ['*']
 
--- deeppavlov/nlp_app/views.py
+++ deeppavlov/nlp_app/views.py
@@ -1,46 +1,47 @@
 from django.shortcuts import render
-from requests import post
+import requests
 
 
 def index(request):
-    data = {"data1": "Здесь будет указано одинаковые/разные значения у текстов",
-            "data2": "Здесь будет указана тональность текстов"}
+    data = {"paraphrase_data": "Здесь будет указано одинаковые/разные значения у текстов",
+            "sentiment": "Здесь будет указана тональность текстов"}
 
-    if request.method == 'POST' and 'btn1' in request.POST:
-        link = "http://127.0.0.1:5001/model"
+    URL_paraphrase_API = "http://192.168.1.71:5001"
+    ENDPOINT = '/model'
+
+    if request.method == 'POST' and 'paraphrase_btn' in request.POST:
         body = {
             "text_a": [
-                request.POST.get('json_data', '')
+                request.POST.get('json_data_for_text_a', '')
             ],
             "text_b": [
-                request.POST.get('json_data2', '')
+                request.POST.get('json_data_for_text_b', '')
             ]
         }
-        responce = post(link, json=body)
+        responce = requests.post(URL_paraphrase_API + ENDPOINT, json=body)
 
         if responce.text[1] == '1':
-            data = {"data1": "Данные два текста имеют одинаковое значение"}
+            data = {"paraphrase_data": "Данные два текста имеют одинаковое значение"}
         elif responce.text[1] == '0':
-            data = {"data1": "Данные два текста разные по своим значениям"}
+            data = {"paraphrase_data": "Данные два текста разные по своим значениям"}
 
         return render(request, 'nlp_app/index.html', data)
 
-    if request.method == 'POST' and 'btn2' in request.POST:
-        link = "http://172.17.0.2:5000"
-        ENDPOINT = "model/"
+    URL_sentiment_API = "http://192.168.1.71:5000"
+    if request.method == 'POST' and 'sentiment_btn' in request.POST:
         body = {
             "x": [
-                request.POST.get('json_data3', '')
+                request.POST.get('json_data_for_sentiment', '')
             ]
         }
-        responce = post(link+"/"+ENDPOINT, json=body)
-        print(responce.text[1])
+        responce = requests.post(URL_sentiment_API + ENDPOINT, json=body)
+
         if responce.text == '["negative"]':
-            data = {"data2": "Негативная тональность текста"}
+            data = {"sentiment": "Негативная тональность текста"}
         elif responce.text == '["positive"]':
-            data = {"data2": "Положительная тональность текста"}
+            data = {"sentiment": "Положительная тональность текста"}
         else:
-            data = {"data2": "Нейтральная тональность текста"}
+            data = {"sentiment": "Нейтральная тональность текста"}
 
         return render(request, 'nlp_app/index.html', data)
 
